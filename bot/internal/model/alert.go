3e11443dcb4610bcd55a9da56a946718ff29d26e
AlexSurfer <artielucifer@gmail.com>
2026-06-03 20:03:09 +0300

    Delete bot directory

--- bot/internal/model/alert.go
+++
@@ -1,8 +0,0 @@
-package model
-
-type AlertRequest struct {
-	ClientUUID string `json:"client_uuid"`
-	IPCount    int    `json:"ip_count"`
-	MaxIPs     int    `json:"max_ips"`
-	Text       string `json:"text"`
-}
